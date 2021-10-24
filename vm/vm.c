b6f7fc1cd1a64bd32e48108af3f142e171072c47
antaechan <0627antaechan@gmail.com>
2021-10-24 07:10:48 +0000

    fix spt_find_page pointer error

--- vm/vm.c
+++ vm/vm.c
@@ -57,15 +57,14 @@ vm_alloc_page_with_initializer (enum vm_type type, void *upage, bool writable,
 	ASSERT (VM_TYPE(type) != VM_UNINIT)
 
 	struct supplemental_page_table *spt = &thread_current ()->spt;
-
+	
 	/* TODO: Create the page */
 	struct page *page = malloc(sizeof(struct page));
 	if(page == NULL)
 		goto error;
-
+	
 	/* Check wheter the upage is already occupied or not. */
 	if (spt_find_page (spt, upage) == NULL) {
-
 		/* TODO: fetch the initialier according to the VM type 
 		 * TODO: and then create "uninit" page struct by calling uninit_new. You
 		 * TODO: should modify the field after calling the uninit_new. */
@@ -97,12 +96,12 @@ error:
 /* Find VA from spt and return page. On error, return NULL. */
 struct page *
 spt_find_page (struct supplemental_page_table *spt UNUSED, void *va UNUSED) {
-	struct page *page = NULL;
+	struct page page;
 	struct hash_elem *e;
 	/* TODO: Fill this function. */
-	page->va = pg_round_down(va);
+	page.va = pg_round_down(va);
 
-	e = hash_find(spt->pages, &page->helem);
+	e = hash_find(spt->pages, &page.helem);
 	return e != NULL ? hash_entry(e, struct page, helem) : NULL;
 }
 
@@ -191,7 +190,7 @@ vm_try_handle_fault (struct intr_frame *f UNUSED, void *addr UNUSED,
 	page = spt_find_page(spt, addr);
 	if(page == NULL)
 		return false;
-
+	
 	return vm_do_claim_page (page);
 }
 
@@ -220,7 +219,7 @@ vm_do_claim_page (struct page *page) {
 	struct thread *t = thread_current();
 	struct frame *frame = vm_get_frame();
 	bool success;
-
+	
 	/* Set links */
 	frame->page = page;
 	page->frame = frame;
