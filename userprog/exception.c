44d8b7ddf54972f2f55b2e3251a4b84e93df384a
antaechan <0627antaechan@gmail.com>
2021-11-06 07:40:30 +0000

    delete page fault print message, Complete mmap-unmap, mmap-ro test cases

--- userprog/exception.c
+++ userprog/exception.c
@@ -155,11 +155,11 @@ page_fault (struct intr_frame *f) {
 	page_fault_cnt++;
 
 	/* If the fault is true fault, show info and exit. */
-	printf ("Page fault at %p: %s error %s page in %s context.\n",
+	/* printf ("Page fault at %p: %s error %s page in %s context.\n",
 			fault_addr,
 			not_present ? "not present" : "rights violation",
 			write ? "writing" : "reading",
-			user ? "user" : "kernel");
+			user ? "user" : "kernel"); */
 	kill (f);
 }
 
